7e6f5b66c449044d9f529d736e28e040dbf39423
n1ck-b <inbolotin@gmail.com>
2024-11-13 23:47:55 +0300

    fix: удалить закомментированный код

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include "MainWindow.h"
 #include <QtWidgets/QApplication>
 #include "Functions.h"
-//#include <QLocale>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
-    /*QLocale::setDefault(QLocale::Russian);*/
     setlocale(LC_ALL, "ru_RU.UTF-8");
     QApplication a(argc, argv);
     MainWindow w;
